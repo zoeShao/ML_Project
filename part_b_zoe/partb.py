bbe0ff2953be10536513ae2c13899953fbd45fbc
Yuting <46699280+zoeShao@users.noreply.github.com>
2020-12-14 20:23:47 +0800

    Added comments for all the functions

--- part_b_zoe/partb.py
+++ part_b_zoe/partb.py
@@ -1,50 +1,65 @@
-from sklearn.impute import KNNImputer
 from utils import *
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
 import pandas as pd
 import numpy as np
 from ast import literal_eval
 from scipy import spatial
 import operator
-from datetime import datetime
 
 
 def pre_process_ques_data():
+    """
+    Pre-process the training dataset to gain the statistical property of
+    the question data.
+
+    :return:
+    """
     correctness = pd.read_csv('../data/train_data.csv', header=0, usecols=range(3))
-    print(correctness.head())
 
-    #  Group everything by question ID, and compute the total number of
-    #  correctness  and the average correctness for every question
+    # Group by question ID, and compute the total number of
+    # correctness and the average correctness for each question
     ques_properties = correctness.groupby('question_id').agg({'is_correct': [np.size, np.mean]})
-    print(ques_properties.head())
+    # print(ques_properties.head())
 
-    # Create a new DataFrame that contains the normalized number of correctness.
-    # So, a value of 0 means nobody did it correctly, and a value of 1 will
-    # mean it's the most easy one to do.
-    ques_num_correctness = pd.DataFrame(ques_properties['is_correct']['mean'])
-    ques_norm_num_correctness = ques_num_correctness.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-    print(ques_norm_num_correctness.head())
+    # Create a new DataFrame that contains the normalized correctness.
+    ques_correctness = pd.DataFrame(ques_properties['is_correct']['mean'])
+    ques_norm_correctness = ques_correctness.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+    # print(ques_norm_correctness.head())
 
-    return ques_properties, ques_num_correctness, ques_norm_num_correctness
+    return ques_properties, ques_correctness, ques_norm_correctness
 
 
 def pre_process_user_data():
+    """
+    Pre-process the training dataset to gain the statistical property of
+    the student data.
+
+    :return:
+    """
     correctness = pd.read_csv('../data/train_data.csv', header=0, usecols=range(3))
-    print(correctness.head())
 
     user_properties = correctness.groupby('user_id').agg({'is_correct': [np.size, np.mean]})
-    print(user_properties.head())
+    # print(user_properties.head())
 
-    user_num_correctness = pd.DataFrame(user_properties['is_correct']['mean'])
-    user_norm_num_correctness = user_num_correctness.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
-    print(user_norm_num_correctness.head())
+    user_correctness = pd.DataFrame(user_properties['is_correct']['mean'])
+    user_norm_correctness = user_correctness.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
+    # print(user_norm_correctness.head())
 
-    return user_properties, user_num_correctness, user_norm_num_correctness
+    return user_properties, user_correctness, user_norm_correctness
 
 
-def collect_question_meta(ques_norm_num_correctness, ques_properties):
+def collect_question_meta(ques_norm_correctness, ques_properties):
+    """
+    Collect more information about each question, like subjects. There are 388
+    distinct subjects. Convert the subject list into an array that contains 388
+    entries. If the question has subject i, then the i-th entry would be 1,
+    and 0 otherwise. Return a dictionary where keys are the question ids.
+
+    :param ques_norm_correctness:
+    :param ques_properties:
+    :return: A dictionary {question_id: tuple}
+    """
     question_dict = {}
     with open(r'../data/question_meta.csv', encoding="ISO-8859-1") as f:
         next(f)
@@ -54,14 +69,22 @@ def collect_question_meta(ques_norm_num_correctness, ques_properties):
             question_id = int(fields[0][:-1])
             subjects = fields[1]
             subjects = literal_eval(subjects)
-
+            # Convert the subject list into an array.
             for subject_id in subjects:
                 subject_lst[subject_id] = 1
-            question_dict[question_id] = (question_id, subjects, np.array(subject_lst), ques_norm_num_correctness.loc[question_id].get('mean'), ques_properties.loc[question_id].is_correct.get('mean'))
+            question_dict[question_id] = (question_id, subjects, np.array(subject_lst), ques_norm_correctness.loc[question_id].get('mean'), ques_properties.loc[question_id].is_correct.get('mean'))
     return question_dict
 
 
-def collect_user_meta(user_norm_num_correctness, user_properties):
+def collect_user_meta(user_norm_correctness, user_properties):
+    """
+    Collect more information about each student, like gender.
+    Return a dictionary where keys are the user ids.
+
+    :param user_norm_correctness:
+    :param user_properties:
+    :return: A dictionary {question_id: tuple}
+    """
     user_dict = {}
     with open(r'../data/student_meta.csv', encoding="ISO-8859-1") as f:
         next(f)
@@ -69,97 +92,159 @@ def collect_user_meta(user_norm_num_correctness, user_properties):
             fields = line.rstrip('\n').split(',')
             user_id = int(fields[0])
             gender = int(fields[1])
-            user_dict[user_id] = (user_id, gender, user_norm_num_correctness.loc[user_id].get('mean'), user_properties.loc[user_id].is_correct.get('mean'))
+            user_dict[user_id] = (user_id, gender, user_norm_correctness.loc[user_id].get('mean'), user_properties.loc[user_id].is_correct.get('mean'))
     return user_dict
 
 
-def compute_ques_distance(a, b):
-    subject_a = a[2]
-    subject_b = b[2]
+def compute_ques_distance(ques_a, ques_b):
+    """
+    Compute the distance between two questions based on how similar
+    their subjects are, and how similar their correctness are.
+
+    :param ques_a: tuple
+    :param ques_b: tuple
+    :return: float
+    """
+    subject_a = ques_a[2]
+    subject_b = ques_b[2]
     subject_distance = spatial.distance.cosine(subject_a, subject_b)
-    correctness_a = a[3]
-    correctness_b = b[3]
+    correctness_a = ques_a[3]
+    correctness_b = ques_b[3]
     correctness_distance = abs(correctness_a - correctness_b)
     return subject_distance + correctness_distance
 
 
-def get_ques_neighbors(question_id, question_dict, K):
+def knn_for_ques(question_id, question_dict, k):
+    """ Use KNN model to predict the correctness for a specific
+    question.
+
+    :param question_id: int
+    :param question_dict: A dictionary {question_id: tuple}
+    :param k: int
+    :return: float
+    """
     distances = []
     for question in question_dict:
         if question != question_id:
             dist = compute_ques_distance(question_dict[question_id], question_dict[question])
             distances.append((question, dist))
     distances.sort(key=operator.itemgetter(1))
+
+    # Find the nearest k neighbours
     neighbors = []
-    for x in range(K):
+    for x in range(k):
         neighbors.append(distances[x][0])
 
     avg_ques_correctness = 0
-
     for neighbor in neighbors:
         avg_ques_correctness += question_dict[neighbor][3]
-    avg_ques_correctness /= K
+    avg_ques_correctness /= k
     return avg_ques_correctness
 
 
-def compute_user_distance(a, b):
-    gender_a = a[1]
-    gender_b = b[1]
+def compute_user_distance(user_a, user_b):
+    """
+    Compute the distance between two students based on how similar
+    their gender are, and how similar their correctness are.
+
+    :param user_a: tuple
+    :param user_b: tuple
+    :return: float
+    """
+    gender_a = user_a[1]
+    gender_b = user_b[1]
     gender_distance = abs(gender_a - gender_b)
-    correctness_a = a[2]
-    correctness_b = b[2]
+    correctness_a = user_a[2]
+    correctness_b = user_b[2]
     correctness_distance = abs(correctness_a - correctness_b)
     return gender_distance + correctness_distance
 
 
-def get_user_neighbors(user_id, user_dict, K):
+def knn_for_user(user_id, user_dict, k):
+    """ Use KNN model to predict the correctness of a specific
+    user.
+
+    :param user_id: int
+    :param user_dict: A dictionary {user_id: tuple}
+    :param k: int
+    :return: float
+    """
     distances = []
     for user in user_dict:
         if user != user_id:
             dist = compute_user_distance(user_dict[user_id], user_dict[user])
             distances.append((user, dist))
     distances.sort(key=operator.itemgetter(1))
+
+    # Find the nearest k neighbours
     neighbors = []
-    for x in range(K):
+    for x in range(k):
         neighbors.append(distances[x][0])
 
     avg_user_correctness = 0
-
     for neighbor in neighbors:
         avg_user_correctness += user_dict[neighbor][2]
-    avg_user_correctness /= K
+    avg_user_correctness /= k
     return avg_user_correctness
 
 
-def predict_ques(sparse_matrix, question_dict, K):
+def predict_all_ques(sparse_matrix, question_dict, k):
+    """ Get the prediction matrix (52x1774) for all questions by using knn
+    to predict each question correctness.
+
+    :param sparse_matrix: 2D sparse matrix
+    :param question_dict: A dictionary {question_id: tuple}
+    :param k: int
+    :return: 2D matrix
+    """
     user_nums, question_nums = sparse_matrix.shape
     question_pred_ll = np.zeros(question_nums)
-    # question_pred_matrix = np.zeros((user_nums, question_nums))
+
     for q_id in range(question_nums):
-        question_pred = get_ques_neighbors(q_id, question_dict, K)
+        question_pred = knn_for_ques(q_id, question_dict, k)
         question_pred_ll[q_id] = question_pred
     question_pred_matrix = np.repeat(np.array([question_pred_ll]), user_nums, axis=0)
     return question_pred_matrix
 
 
-def predict_user(sparse_matrix, user_dict, K):
+def predict_all_user(sparse_matrix, user_dict, k):
+    """
+    Get the prediction matrix (52x1774) for all students by using knn
+    to predict each student correctness.
+
+    :param sparse_matrix: 2D sparse matrix
+    :param user_dict: A dictionary {user_id: tuple}
+    :param k: int
+    :return: 2D matrix
+    """
     user_nums, question_nums = sparse_matrix.shape
     user_pred_ll = []
-    # user_pred_matrix = np.zeros((user_nums, question_nums))
+
     for u_id in range(user_nums):
-        user_pred = get_user_neighbors(u_id, user_dict, K)
+        user_pred = knn_for_user(u_id, user_dict, k)
         user_pred_ll.append(user_pred)
     user_pred_matrix = np.repeat(np.array([user_pred_ll]), question_nums, axis=0).T
     return user_pred_matrix
 
 
-def predict_all(sparse_matrix, val_data, question_dict, user_dict, K):
-    question_pred_matrix = predict_ques(sparse_matrix, question_dict, K)
-    user_pred_matrix = predict_user(sparse_matrix, user_dict, K)
+def predict_all(sparse_matrix, val_data, question_dict, user_dict, k):
+    """
+    Get the prediction matrix (52x1774) for the sparse matrix by averaging the
+    prediction over the user_pred_matrix and question_pred_matrix.
+    Return the accuracy on valid_data.
 
+    :param sparse_matrix: 2D sparse matrix
+    :param val_data: A dictionary {user_id: list, question_id: list,
+    is_correct: list}
+    :param question_dict: A dictionary {question_id: tuple}
+    :param user_dict: A dictionary {user_id: tuple}
+    :param k: int
+    :return: float
+    """
+    question_pred_matrix = predict_all_ques(sparse_matrix, question_dict, k)
+    user_pred_matrix = predict_all_user(sparse_matrix, user_dict, k)
     final_pred_matrix = (question_pred_matrix + user_pred_matrix)/2
 
-    # print(final_pred_matrix)
     pred_matrix = sparse_matrix.copy()
     pred_matrix[np.isnan(sparse_matrix)] = final_pred_matrix[np.isnan(sparse_matrix)]
 
@@ -172,21 +257,22 @@ def main():
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
 
-    ques_properties, ques_num_correctness, ques_norm_num_correctness = pre_process_ques_data()
-    user_properties, user_num_correctness, user_norm_num_correctness = pre_process_user_data()
+    print("Calculating...")
+    ques_properties, ques_correctness, ques_norm_correctness = pre_process_ques_data()
+    user_properties, user_correctness, user_norm_correctness = pre_process_user_data()
 
-    question_dict = collect_question_meta(ques_norm_num_correctness, ques_properties)
-    user_dict = collect_user_meta(user_norm_num_correctness, user_properties)
+    question_dict = collect_question_meta(ques_norm_correctness, ques_properties)
+    user_dict = collect_user_meta(user_norm_correctness, user_properties)
     # Examples:
     # print(question_dict[1])
     # print(user_dict[1])
 
     valid_accu_values = []
     k_values = list(range(1, 10, 2))
-    for K in range(1, 10, 2):
+    for k in range(1, 10, 2):
         print('---------------')
-        print("When k = {}:".format(K))
-        valid_accu = predict_all(sparse_matrix, val_data, question_dict, user_dict, K)
+        print("When k = {}:".format(k))
+        valid_accu = predict_all(sparse_matrix, val_data, question_dict, user_dict, k)
         print("Validation Accuracy: {}".format(valid_accu))
         valid_accu_values.append(valid_accu)
 
